9d92320a252197378ebb963e79dd441c631e8a66
gautier <gautier>
2008-03-01 10:14:41 +0000

    script de mise à jour des fichiers po pour pootle

--- pmbdoc/update_po.sh
+++ pmbdoc/update_po.sh
@@ -3,6 +3,6 @@
 # A placer dans le répertoire avec les fichiers po à mettre à jour
 for fic in *.po
 do
-    echo -n Mis à jour de $fic ...
-    po4a-updatepo -f docbook -m /home/gmichelin/workspace/pmbdoc/fr_FR/user/${fic%.*}.xml -p ${fic%.*}.po 
+    echo -n Mise à jour de $fic ...
+    po4a-updatepo -f docbook -m ~/workspace/pmbdoc/fr_FR/user/${fic%.*}.xml -p ${fic%.*}.po 
 done
